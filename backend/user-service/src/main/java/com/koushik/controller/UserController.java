72db85411703b9fc96740dff1c15827be88a54d9
Gurumadwa Koushik <madwaoffical@gmail.com>
2026-03-21 18:18:43 +0530

    user crud operations

---
+++ backend/user-service/src/main/java/com/koushik/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.koushik.controller;
+
+import com.koushik.model.User;
+import com.koushik.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/api/users")
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Long id) throws Exception {
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isPresent()){
+            return opt.get();
+        }
+        throw new Exception("User not present!");
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id){
+        Optional<User> opt = userRepository.findById(id);
+
+        if(opt.isEmpty()){
+            throw new RuntimeException("User not found with id "+id);
+        }
+        User existingUser = opt.get();
+
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    public String deleteUser(@PathVariable Long id){
+        Optional<User> opt = userRepository.findById(id);
+        if(opt.isEmpty())
+            throw new RuntimeException("User Not found for id "+id);
+
+        userRepository.deleteById(opt.get().getId());
+        return "User deleted successfully!";
+    }
+
+}
---
+++ backend/user-service/src/main/java/com/koushik/model/User.java
@@ -0,0 +1,32 @@
+package com.koushik.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String password;
+    private String email;
+    private String phone;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
